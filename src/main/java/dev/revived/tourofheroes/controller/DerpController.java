89f817c1f30b4d82e25557cb28bd0f68b861e0ca
Seamus Riordan <seamus@riordan.io>
2020-05-08 14:11:50 -0500

    Playing around

--- src/main/java/dev/revived/tourofheroes/controller/DerpController.java
+++ src/main/java/dev/revived/tourofheroes/controller/DerpController.java
@@ -1,17 +1,64 @@
 package dev.revived.tourofheroes.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/derp")
 public class DerpController {
+    class Garbage {
+        public Garbage() {
+        }
 
+        private String thing = "stuff";
+
+        public Integer r = -47;
+
+        public String getThing() {
+            return thing;
+        }
+
+        public Float getF() {
+            return 60.f;
+        }
+
+        public Float getF(double f) {
+            return (float) f;
+        }
+    }
+
+    @Autowired
     public DerpController() {
     }
 
-    @GetMapping(value = "/derp")
+    Logger logger = LoggerFactory.getLogger(DerpController.class);
+
+
+    @GetMapping("")
     public String gogogogo() {
 
         return "assasssasss";
     }
+
+    @GetMapping("/json")
+    public String json() {
+
+        String v = "";
+        try {
+            v = new ObjectMapper()
+                    .writeValueAsString(new Garbage());
+            logger.debug(String.format("Derp  %s", v));
+
+        } catch (java.io.IOException e) {
+            logger.error(String.format("Derpfail %s", e.getMessage()));
+        }
+        return v;
+    }
+
+
 }
---
+++ src/main/java/dev/revived/tourofheroes/controller/FrumPageController.java
@@ -0,0 +1,25 @@
+package dev.revived.tourofheroes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/frum")
+public class FrumPageController {
+    public FrumPageController() {
+    }
+
+    @RequestMapping("")
+    public @ResponseBody
+    String frum(@RequestParam(value = "frumid", required = false, defaultValue = "") String id) {
+        return "<html><head><title>WELOME TO FRUM</title> <link rel=\"stylesheet\" type=\"text/css\" href=\"frum.css\"></head><body><div class=\"frum\">frum" + id + "</div></body></html>";
+    }
+
+    @RequestMapping("/grib")
+    public String grib(Model model) {
+        model.addAttribute("gribkey", "gribviewm");
+        return "gribview";
+    }}
